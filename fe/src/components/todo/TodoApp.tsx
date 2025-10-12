d121a587c1da4c47605fd2751810fb09889bf990
Arthur Hozanna <90093341+arthurhzna@users.noreply.github.com>
2025-10-12 16:45:24 +0700

    feat: add some route

--- fe/src/components/todo/TodoApp.tsx
+++ fe/src/components/todo/TodoApp.tsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 
 import HeaderComponent from './HeaderComponent.tsx'
 import LoginComponent from './LoginComponent.tsx'
+import ErrorComponent from './ErrorComponent.tsx'
 
 export default function TodoApp() {
     return (
@@ -10,6 +11,12 @@ export default function TodoApp() {
             <HeaderComponent />
                 <Routes>
                     <Route path="/" element={<LoginComponent />} />
+                    <Route path="/login" element={<LoginComponent />} />
+                    <Route path="/welcome/:username" element={<WelcomeComponent />} />
+                    <Route path="/todos" element={<ListTodosComponent />} />
+                    <Route path="/todo/:id" element={<TodoComponent />} />
+                    <Route path="/logout" element={<LogoutComponent />} />
+                    <Route path='*' element={<ErrorComponent /> } />
                 </Routes>
             </BrowserRouter>
             {/* <h1>Todo App</h1> */}
